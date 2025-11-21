f99ce6bde8e93bb62b3212fdcfcc3916bb6b6be7
a25vigsu <viggosundin03@gmail.com>
2025-11-21 15:05:21 +0100

    added scene with background story

---
+++ Assets/Scripts/IntroChange.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IntroChange : MonoBehaviour
+{
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
